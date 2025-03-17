55cd98091037c1b6d39c13e577fc5e0e3b6be595
sudatra <mail2sudatra@gmail.com>
2025-03-17 20:45:20 +0530

    added header Name stying

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,16 +6,20 @@ import HeaderPulse from './HeaderPulse'
 
 const Header = () => {
   return (
-    <header className='sticky top-0 z-50 left-0 right-0'>
-      <div>
-        <div>
-          <Link href='/'>
+    <header className='sticky top-0 z-50 left-0 right-0 px-4 md:px-0 bg-white/80 backdrop-blur-sm 
+    border-b border-gray-200'>
+      <div className='container mx-auto'>
+        <div className='flex items-center justify-between h-16'>
+          <Link 
+            href='/'
+            className='flex gap-4 items-center'
+          >
             <HeaderPulse 
               size='small'
               color='blue'
             />
 
-            <h1>AI SaaS</h1>
+            <h1 className='text-xl font-semibold bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent'>AI SaaS</h1>
           </Link>
         </div>
 
