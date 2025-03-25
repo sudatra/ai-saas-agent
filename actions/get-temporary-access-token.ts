4b6e5c8c3a45d4169fb255fb20115e8f131fb908
sudatra <mail2sudatra@gmail.com>
2025-03-25 20:07:16 +0530

    added server action for schematic access-token fetch

--- actions/get-temporary-access-token.ts
+++ actions/get-temporary-access-token.ts
@@ -1,1 +1,27 @@
 'use server'
+
+import { currentUser } from "@clerk/nextjs/server";
+import { SchematicClient } from "@schematichq/schematic-typescript-node";
+
+const apiKey = process.env.SCHEMATIC_API_apiKey;
+if(!apiKey) {
+  throw new Error("Api key for schematic not found");
+}
+
+const client = new SchematicClient({
+  apiKey: apiKey
+});
+
+export async function getTemporaryAccessToken() {
+  const user = await currentUser();
+  if(!user) {
+    return null;
+  }
+
+  const response = await client.accesstokens.issueTemporaryAccessToken({
+    resourceType: 'company',
+    lookup: { id: user.id }
+  });
+
+  return response.data.token;
+}
--- app/components/schematic/SchematicComponent.tsx
+++ app/components/schematic/SchematicComponent.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import SchematicEmbed from './SchematicEmbed'
+import { getTemporaryAccessToken } from '@/actions/get-temporary-access-token'
 
 const SchematicComponent = async ({ componentId }: { componentId: string }) => {
-  
+  if(!componentId) {
+    return null;
+  }
+
+  const accessToken = await getTemporaryAccessToken();
+  if(!accessToken) {
+    throw new Error('Failed to fetch access token');
+  }
 
   return (
     <SchematicEmbed 
-
+      accessToken={accessToken}
+      componentId={componentId}
     />
   )
 }
