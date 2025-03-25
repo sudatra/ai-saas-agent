4420a5483f5934f594f897065ea888e98dd2410f
sudatra <mail2sudatra@gmail.com>
2025-03-25 19:58:35 +0530

    added schematic embed and wrapper components

---
+++ app/components/schematic/SchematicComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SchematicEmbed from './SchematicEmbed'
+
+const SchematicComponent = async ({ componentId }: { componentId: string }) => {
+  
+
+  return (
+    <SchematicEmbed 
+
+    />
+  )
+}
+
+export default SchematicComponent
---
+++ app/components/schematic/SchematicEmbed.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { SchematicEmbed as SchematicEmbedComponent } from "@schematichq/schematic-components";
+
+const SchematicEmbed = ({ accessToken, componentId }: { accessToken: string, componentId: string }) => {
+  return (
+    <SchematicEmbedComponent 
+      accessToken={accessToken}
+      id={componentId}
+    />
+  )
+}
+
+export default SchematicEmbed;
