126fdcfcae627fea52c9089d26ce59169a74e866
sudatra <mail2sudatra@gmail.com>
2025-04-02 19:53:16 +0530

    added layout styling for analysis page

--- app/video/[videoId]/analysis/page.tsx
+++ app/video/[videoId]/analysis/page.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const AnalysisPage = () => {
   return (
-    <div>
-      ap
+    <div className='xl:container mx-auto px-4 md:px-0'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+        <div className='order-2 lg:order-1 flex flex-col gap-2 bg-white lg:border-r border-gray-200 p-6'>
+
+        </div>
+
+        <div className='order-1 lg:order-2 lg:sticky lg:top-20 h-[500px] md:h-[calc(100vh-6rem)]'>
+
+        </div>
+      </div>
     </div>
   )
 }
