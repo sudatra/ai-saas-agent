38191de24252125dacbce255009d84576b29832b
sudatra <mail2sudatra@gmail.com>
2025-04-04 21:21:15 +0530

    added video thumbnail and channel details ui

--- app/components/YoutubeVideoDetails.tsx
+++ app/components/YoutubeVideoDetails.tsx
@@ -2,6 +2,7 @@
 
 import { getVideoDetails } from '@/actions/get-video-details';
 import { VideoDetails } from '@/types/types';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
 const YoutubeVideoDetails = ({ videoId }: { videoId: string }) => {
@@ -20,11 +21,38 @@ const YoutubeVideoDetails = ({ videoId }: { videoId: string }) => {
     return <div>Video not found</div>
   }
 
-  console.log(video)
-
   return (
-    <div>
+    <div className='@container bg-white rounded-xl'>
+      <div className='flex flex-col gap-8'>
+        <div className='flex-shrink-0'>
+          <Image 
+            src={video.thumbnail}
+            alt={video.title}
+            width={500}
+            height={500}
+            className='w-full rounded-xl shadow-md hover:shadow-xl transition-shadow duration-300'
+          />
+        </div>
 
+        <div className='flex-grow space-y-4'>
+          <h1 className='text-2xl @lg:text-3xl font-bold text-gray-900 leading-tight line-clamp-2'>{video.title}</h1>
+        </div>
+
+        <div className='flex items-center gap-4'>
+          <Image 
+            src={video.channel.thumbnail}
+            alt={video.channel.title}
+            width={48}
+            height={48}
+            className='w-10 h-10 @md:h-12 rounded-full border-2 border-gray-100'
+          />
+
+          <div>
+            <p className='text-base @md:text-lg font-semibold text-gray-900'>{video.channel.title}</p>
+            <p className='text-sm @md:text-base text-gray-600'>{video.channel.subscribers} subscribers</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
           <main>
             {children}
           </main>
-
         </ClientWrapper>
       </body>
     </html>
