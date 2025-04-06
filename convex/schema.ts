abd96f4fc54eba85da9deebfa90325bc470b57c2
sudatra <mail2sudatra@gmail.com>
2025-04-06 17:26:31 +0530

    added convex setup and convex database schema

---
+++ convex/schema.ts
@@ -0,0 +1,48 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from 'convex/values';
+
+export default defineSchema({
+  videos: defineTable({
+    videoId: v.string(),
+    userId: v.string()
+  })
+    .index('by_user_id', ['userId'])
+    .index('by_video_id', ['videoId'])
+    .index('by_user_and_video', ['userId', 'videoId'])
+  ,
+
+  transcript: defineTable({
+    videoId: v.string(),
+    userId: v.string(),
+    transcript: v.array(
+      v.object({
+        text: v.string(),
+        timeStamp: v.string()
+      })
+    )
+  })
+    .index('by_user_id', ['userId'])
+    .index('by_video_id', ['videoId'])
+    .index('by_user_and_video', ['userId', 'videoId'])
+  ,
+
+  images: defineTable({
+    storageId: v.id('_storage'),
+    userId: v.string(),
+    videoId: v.string()
+  })
+    .index('by_user_id', ['userId'])
+    .index('by_video_id', ['videoId'])
+    .index('by_user_and_video', ['userId', 'videoId'])
+  ,
+
+  titles: defineTable({
+    videoId: v.string(),
+    userId: v.string(),
+    title: v.string()
+  })
+    .index('by_user_id', ['userId'])
+    .index('by_video_id', ['videoId'])
+    .index('by_user_and_video', ['userId', 'videoId'])
+  ,
+})
