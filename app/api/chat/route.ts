b3703e5c3ec5e30f93428a413a2ce34688b1f212
sudatra <mail2sudatra@gmail.com>
2025-04-08 19:57:23 +0530

    added anthropic setup and api route setup for LLM call

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,21 @@
+import { createAnthropic } from "@ai-sdk/anthropic";
+import { streamText } from "ai";
+import { NextResponse } from "next/server";
+
+const anthropic = createAnthropic({
+  apiKey: process.env.CLAUDE_API_KEY,
+  headers: {
+    'anthropic-beta': 'token-efficient-tools-2025-02-19'
+  }
+});
 
 export async function POST(req: Request) {
-  
+  const { messages, videoId } = await req.json();
+  const model = anthropic('claude-3-7-sonnet-20250219');
+  const result = streamText({
+    model: model,
+    messages: messages,
+  });
+
+  return result.toDataStreamResponse();
 }
